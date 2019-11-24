ba8436c537c40b41f654defd595329c334f90aa7
duan2580 <835772417@qq.com>
2019-11-24 09:23:22 +0800

    使用element-ui

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+// 导入某一个目录,默认会去找索引文件(index.js index.vue index.json)  基于webpack
+// 2.@ 是一个别名 指定的是 /src 路径 一个绝对路径 如果不是vue-cli项目不能用@ 基于webpack
+import router from '@/router'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 // 根实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
 
